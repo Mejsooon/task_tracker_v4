2afcb69010cd3d1b1b5460cf6a0bf6b6b74149fe
Mikołaj <mikolajfrak.priv@gmail.com>
2026-04-14 12:34:02 +0200

    Made some changes

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,4 +1,15 @@
 
 
+def multiline_read(prompt: str = ""):
+    if prompt:
+        print(prompt)
+    lines = []
+    while True:
+        line = input()
+        if not line:
+            break
+        lines.append(line)
+    return "\n".join(lines)
+
 def clear_screen():
     pass
