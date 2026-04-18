d8265eb29f28efc6d84c4d540c35612bc8b704d4
Mikołaj <mikolajfrak.priv@gmail.com>
2026-04-18 15:20:02 +0200

    Created new get_next_id() function

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,8 +2,16 @@ def clear_screen():
     print("\n" * 10)
 
 
-def get_next_id(prefix: str, items: list):
-    pass
+def get_next_id(prefix: str, table: list):
+    from app.core.database import execute
+    row = execute(f"SELECT id FROM {table} ORDER BY id DESC LIMIT 1", fetch="one")
+    # Wynikiem tego zapytania będzie słownik np: {"id": U003}
+
+    if not row:
+        return f"{prefix}001"
+
+    number = int(row["id"][len(prefix):]) # row["id"] wywołuje wartośc przypisaną do tego klucza -> U003
+    return f"{prefix}{number + 1:03d}"
 
 
 def read_multiline(prompt: str):
