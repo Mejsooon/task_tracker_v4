78b6f0f338815b679da085ee66b42de66a76dea0
Mikołaj <mikolajfrak.priv@gmail.com>
2026-04-28 21:11:33 +0200

    Change: modified to SQLAlchemy

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,16 +1,21 @@
+from app.core.database import get_session
+from sqlalchemy import text
+
+
 def clear_screen():
     print("\n" * 10)
 
 
-def get_next_id(prefix: str, table: list):
-    from app.core.database import execute
-    row = execute(f"SELECT id FROM {table} ORDER BY id DESC LIMIT 1", fetch="one")
-    # Wynikiem tego zapytania będzie słownik np: {"id": U003}
+def get_next_id(prefix: str, table: str) -> str:
+    with get_session() as session:
+        row = session.execute(
+            text(f"SELECT id FROM {table} ORDER BY id DESC LIMIT 1")
+        ).fetchone()
 
     if not row:
         return f"{prefix}001"
 
-    number = int(row["id"][len(prefix):]) # row["id"] wywołuje wartośc przypisaną do tego klucza -> U003
+    number = int(row[0][len(prefix):])
     return f"{prefix}{number + 1:03d}"
 
 
